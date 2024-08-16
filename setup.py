e9ec49d168f2b5b40202feeb85b1cdcf096c371d
Barbora Kindlova <kindlova.barbora12@gmail.com>
2024-08-17 01:35:30 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="automatize_text_process",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "columns", "main"
+    ],
+)
